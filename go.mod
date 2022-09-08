66e48bde5c4054af8002a5f08998a9d685e1593e
Luis Enrique Landaure Vargas <lelandaure@gmail.com>
2022-09-07 23:35:52 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lelandaure/testing-in-go
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/lelandaure/testing-in-go/controller"
+	"net/http"
+)
+
+var port = ":8080"
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/pokemon/{id}", controller.GetPokemon).Methods("GET")
+
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		fmt.Print("Error found")
+	}
+	fmt.Println("Listening on port", port)
+}
